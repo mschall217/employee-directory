e6303c8cfd3a59012fb420d08cb4963c8e68078f
Morgan Schall <morgan.allison.schall@gmail.com>
2021-06-21 11:56:59 -0400

    Updates

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const URL = "https://randomuser.me/api/?results=20&nat=us"
-// eslint-disable-next-line 
-export default {
-    getEmp: function() {
-        return axios.get(URL);
-    }
-}
