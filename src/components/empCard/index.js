a731483cf1fc0a6dc2b1ad0fe888008739561fc7
Morgan Schall <morgan.allison.schall@gmail.com>
2021-06-21 10:36:09 -0400

    cleaned up components called API

--- src/components/empCard/index.js
+++ src/components/empCard/index.js
@@ -17,15 +17,40 @@ const EmpCard = (props) => {
 						<span onClick={() => props.sortBy("email")}>Email</span>					
 					</th>
 					<th scope="col">
-						<span onClick={() => props.sortBy("place", "city", "state")}>Place</span>					
+						<span onClick={() => props.sortBy("location", "city", "state")}>Place</span>					
 					</th>
 				</tr>
 			</thead>
 			<tbody>
-				
+				{props.state.filterEmployee.map((employee) => {
+					const { first, last } = employee.name;
+					const fullName = `${first} ${last}`;
+
+					const { city, state } = employee.location;
+					const location = `${city}, ${state}`;
+
+					return(
+						<tr>
+							<td>
+								<img src={employee.picture.thumbnail} />
+							</td>
+							<td>
+								{fullName}
+							</td>
+							<td>
+								<a href={`tel:+1${employee.phone}`}>{employee.phone}</a>
+							</td>
+							<td>
+								<a href={`mailto:${employee.email}`}>{employee.email}</a>
+							</td>
+							<td>
+								{location}
+							</td>
+						</tr>
+					)
+				})}
 			</tbody>
 		</table>
-
 	)
 }
 
--- src/components/empData/index.js
+++ src/components/empData/index.js
@@ -1,6 +1,129 @@
 import React, {Component } from "react";
-import "./style.css"
+import Nav from "../Nav"
+import EmpCard from "../EmpCard"
+import API from "../../utils/API"
 
-function empBody ({ users }){
 
-}
+class EmpData extends Component {
+    state = {
+      search: "",
+      employees: [],
+      filteredEmp: [],
+      sortDir: this.initialSortDir,
+    };
+  
+    get initialSortDir() {
+      return {
+        name: "",
+        phone: "",
+        email: "",
+        location: "",
+      };
+    }
+  
+    // When this component mounts, load random users as employees from https://randomuser.me/
+    componentDidMount() {
+      API.getEmp()
+        .then((res) =>
+          this.setState({
+            employees: res.data.results,
+            filteredEmps: res.data.results,
+          })
+        )
+        .catch((err) => console.log(err));
+    }
+  
+    // Update search state to filter by employee name
+    handleInputChange = (event) => {
+      const value = event.target.value;
+      this.setState({ search: value });
+      this.filterEmp(value.toLowerCase().trim());
+    };
+  
+    handleFormSubmit = (event) => {
+      event.preventDefault();
+    };
+  
+    // Sort with the key of specified object.
+    // If key has more than one option sort by primary and optionally a secondary i.e. sort by last name, then first.
+    sortBy = (key, primary = 0, secondary = 0) => {
+      let sortedEmp = this.state.filteredEmp;
+      if (this.state.sortDir[key]) {
+        this.setState({
+          filteredEmp: sortedEmp.reverse(),
+          sortDir: {
+            ...this.initialSortDir,
+            [key]: this.state.sortDir[key] === "asc" ? "desc" : "asc",
+          },
+        });
+      } else {
+        sortedEmp = this.state.filteredEmp.sort((a, b) => {
+          a = a[key];
+          b = b[key];
+  
+          // If primary comparison is equal, sort by secondary
+          if (primary) {
+            if (secondary && a[primary] === b[primary]) {
+              return a[secondary].localeCompare(b[secondary]);
+            }
+            return a[primary].localeCompare(b[primary]);
+          } else {
+            return a.localeCompare(b);
+          }
+        });
+  
+        this.setState({
+          filteredEmp: sortedEmp,
+          sortDir: {
+            ...this.initialSortDir,
+            [key]: "asc",
+          },
+        });
+      }
+    };
+  
+    filterEmp = (input) => {
+      if (input) {
+        this.setState({
+          filteredEmp: this.state.employees.filter((employee) => {
+            return (
+              employee.name.first
+                .toLowerCase()
+                .concat(" ", employee.name.last.toLowerCase())
+                .includes(input) ||
+              employee.phone.includes(input) ||
+              employee.phone.replace(/[^\w\s]/gi, "").includes(input) ||
+              employee.email.includes(input) ||
+              employee.location.city
+              .toLowerCase()
+              .concat(" ", employee.location.state.toLowerCase())
+              .includes(input)
+            );
+          }),
+        });
+      } else {
+        this.setState({ filteredEmp: this.state.employees });
+      }
+    };
+
+    render() {
+        return (
+            <>
+            <Nav 
+              value={this.state.search}
+              handleInputChange={this.handleInputChange}
+              handleFormSubmit={this.handleFormSubmit}
+            />
+            <div className="container mt-4">
+              <EmpCard
+                  state={this.state}
+                  sortBy={this.sortBy}
+                  filterEmp={this.filterEmp}
+              />
+            </div>
+            </>
+        )
+    }
+  };
+
+export default EmpData;
